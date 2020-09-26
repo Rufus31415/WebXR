52c39c3660b166435f57bc9120ab8fe4b0ae6db6
dddd <ddd>
2020-09-26 12:18:31 +0200

    add SimpleWebXR plugin and implement first interactions

---
+++ XRTK.WebXR/Packages/com.xrtk.webxr/Runtime/Plugins/WebXRAPI.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) XRTK. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Rufus31415.WebXR;
+using UnityEngine;
+
+namespace XRTK.WebXR.Plugins
+{
+    public static class WebXRAPI
+    {
+        private static SimpleWebXR _instance;
+
+        // Get the xr session MonoBehavior or create it
+        public static SimpleWebXR GetSession()
+        {
+            if (_instance) return _instance;
+
+            var instance = SimpleWebXR.GetInstance();
+
+            if (instance)
+            {
+                _instance = instance;
+                return instance;
+            }
+
+            var gameObject = new GameObject("WebXR");
+            _instance = gameObject.AddComponent<SimpleWebXR>();
+            return _instance;
+        }
+    }
+}
